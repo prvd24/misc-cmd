d00db43396beee9efcd8bf0eedbd4c04174e772b
Praveen <174160598+prvd24@users.noreply.github.com>
2025-04-18 22:57:52 +0530

    Create windows.cmd

---
+++ windows.cmd
@@ -0,0 +1,6 @@
+rclone sync C:\Users\Praveen\UPSC\ D:\UPSC\ --progress --local-no-sparse
+rclone sync C:\Users\Praveen\Music\ D:\Music\ --progress --local-no-sparse
+
+Start-Process -WindowStyle hidden rclone "mount drive2: F:"
+
+  
